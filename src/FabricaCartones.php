13732e9ed4f6a4be329e264857b90755420b75cc
Amadeo Mendez <amadeo.mendez@outlook.com>
2019-06-11 17:00:53 -0300

    Update 4 +add

--- src/FabricaCartones.php
+++ src/FabricaCartones.php
@@ -2,45 +2,6 @@
 
 namespace Bingo;
 
-class Carton {
-  
-  protected $numeros_carton;
-
-  public function __construct( $numeros ) {
-    foreach ( $numeros as $fila ) {
-      array_push($this->numeros_carton, $fila);
-    }
-  }
-
-  public function filas() {
-    $res = [];
-    foreach ( $this->numeros_carton as $fila ) {
-      array_push($res, $fila);
-    } return $res;
-  }
-
-    public function columnas() {
-      return array_map(null, ...$this->numeros_carton);
-    }
-  
-    public function numerosDelCarton() {
-      $numeros = [];
-      foreach ($this->filas() as $fila) {
-        foreach ($fila as $celda) {
-          if ($celda != 0) {
-            $numeros[] = $celda;
-          }
-        }
-      }
-      return $numeros;
-    }
-
-    public function tieneNumero(int $numero) {
-      return in_array($numero, $this->numerosDelCarton());
-    }
-
-}
-
 class FabricaCartones {
 
   public function generarCarton() {
